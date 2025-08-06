2d2ca890d19a44c5ec77b4bc1bd0084235add044
Shmuel kastro <shmulik.kastro@gmail.com>
2025-08-06 16:01:44 +0300

    שינוי לינק בדף בטיטחת

--- src/app/company-policies/page.tsx
+++ src/app/company-policies/page.tsx
@@ -5,7 +5,6 @@ const policies = [
   { label: 'הקוד האתי', url: 'https://renuar.b-salesbook.com/ImageView/RenderLocalImage?imageUrl=%2FInfoCenter%2F637170421472956625.pdf', color: 'bg-blue-200 hover:bg-blue-300 text-blue-900' },
   { label: 'מניעת הטרדה מינית', url: 'https://renuar.b-salesbook.com/ImageView/RenderLocalImage?imageUrl=%2FInfoCenter%2F638900757766805046.pdf', color: 'bg-green-200 hover:bg-green-300 text-green-900' },
   { label: 'חוברת בטיחות', url: 'https://renuar.b-salesbook.com/ImageView/RenderLocalImage?imageUrl=%2FInfoCenter%2F637170423593303574.pdf', color: 'bg-yellow-200 hover:bg-yellow-300 text-yellow-900' },
-  // { label: 'פתיחת תעודות חוסר/עודף לאריזות שלמות', url: 'https://renuar.b-salesbook.com/ImageView/RenderLocalImage?imageUrl=%2FInfoCenter%2F638548418456129505.pdf', color: 'bg-purple-200 hover:bg-purple-300 text-purple-900' },
   { label: 'תדריך נגישות', url: 'https://renuar.b-salesbook.com/ImageView/RenderLocalImage?imageUrl=%2FInfoCenter%2F638900759615567045.pdf', color: 'bg-teal-200 hover:bg-teal-300 text-teal-900' },
 ];
 
